538f5a974876ed02f19f043bb4d1101664c40dd2
Vighnesh Subramaniam <vsub851@coats.csail.mit.edu>
2021-08-23 17:18:24 -0400

    Finished POS pipeline with all new components

--- postag/postag_main.py
+++ postag/postag_main.py
@@ -31,7 +31,7 @@ def get_cmd_arguments():
 	ap.add_argument('-d', '--data', action = 'store', type = str, dest = 'data_path', default = '/storage/vsub851/typ_embed/datasets', 
 		help = 'Dataset location')
 	ap.add_argument('-t', '--train', action = 'store', type = bool, dest = 'train_model', default = False,
-		help = 'Train a new model, saved in saved_models directory in depparse directory')
+		help = 'Train a new model, saved in saved_models directory in postag directory')
 	ap.add_argument('-m', '--model', action = 'store', type = str, dest = 'modelname', default = 'dep_model.pt', 
 		help = 'Name of saved model that is either being trained or being evaluated. Most be stored in saved_models directory')
 	ap.add_argument('-l', '--lang', action = 'store', type = str, dest = 'lang', default = 'en',
@@ -45,15 +45,15 @@ def get_cmd_arguments():
 	ap.add_argument('-e', '--encoder', action = 'store', type = str, dest = 'encoder', default = 'lstm',
 		help = 'Word Embedding model, either BERT or LSTM')
 	ap.add_argument('-te', '--typencode', action = 'store', typ = str, dest = 'typ_encode', default = 'concat',
-		help = 'Method to use for incorporating typological features. Choose from [concat, add_att, mul_att] to decide to either use a concatentation or attention method')
+		help = 'Method to use for incorporating typological features. Choose from /[concat, add_att, mul_att]/ to decide to either use a concatentation or attention method')
 
 	#Model Hyperparameters
 	ap.add_argument('-wes', '--wordsize', action = 'store', dest = 'word_embed_size', type = int, default = 100, 
 		help = 'Word Embedding Size for model')
-	ap.add_argument('-pes', '--possize', action = 'store', dest = 'pos_embed_size', type = int, default = 100,
-		help = 'POS Embedding Size for model')
 	ap.add_argument('-lhs', '--lstmsize', action = 'store', dest = 'lstm_hidden_size', type = int, default = 400, 
 		help = 'LSTM Hidden size when using encoder LSTM')
+	ap.add_argument('-mhs', '--mlpsize', action = 'store', dest = 'mlp_hidden_size', type = int, default = 200,
+		help = 'Hidden size in MLP')
 	ap.add_argument('-ahs', '--attentionsize', action = 'store', dest = 'attention_hidden_size', type = int, default = 200,
 		help = 'Multiplicative Attention Hidden Size')
 	ap.add_argument('-ll', '--lstmlayers', action = 'store', dest = 'lstm_layers', type = int, default = 3,
@@ -68,8 +68,6 @@ def get_cmd_arguments():
 		help = 'Number of typological features in the typological features extracted.')
 	ap.add_argument('-bl', '--bertlayer', action = 'store', dest = 'bert_layer', type = int, default = 8,
 		help = 'Layer to obtain BERT representations from')
-	ap.add_argument('-sc', '--scale', action = 'store', dest = 'scale', type = float, default = 0,
-		help = 'Scaling factor for biaffine attention')
 	ap.add_argument('-lr', '--learningrate', action = 'store', dest = 'lr', type = float, default = 0.0005, 
 		help = 'Learning rate for optimization')
 	ap.add_argument('-ep', '--numepochs', action = 'store', dest = 'num_epochs', type = int, default = 10,
@@ -86,10 +84,10 @@ def dep_main(train_filename,
 	train_model,
 	input_type,
 	word_embed_size,
-	pos_embed_size,
 	modelname,
 	encoder,
 	lstm_hidden_size,
+	mlp_hidden_size,
 	lr,
 	dropout,
 	num_epochs,
@@ -97,7 +95,6 @@ def dep_main(train_filename,
 	batch_size,
 	bert,
 	bert_layer,
-	scale,
 	typological,
 	typ_embed_size,
 	num_typ_features,
@@ -105,20 +102,20 @@ def dep_main(train_filename,
 	typ_encode,
 	attention_hidden_size,
 	device):
-	print('Starting Dependency Parsing')
+	print('Starting POS tagging')
 
 	print('Loading data in language {} from training file {}, validation file {}, and testing file {}'.format(lang, train_filename, valid_filename, test_filename))
 
 	file_path = os.path.join(data_path, directory)
 	train_lines = preproc_conllu(file_path, filename = train_filename)
 	train_sent_collection = sentence_collection(train_lines)
-	train_corpus, vocab_dict, label_dict, pos_dict = process_corpus(train_sent_collection, mode = 'train', input_type = input_type)
+	train_corpus, vocab_dict, label_dict= process_corpus(train_sent_collection, mode = 'train', input_type = input_type)
 
 	if train_model:
 		valid_lines = preproc_conllu(file_path, filename = valid_filename)
 		valid_sent_collection = sentence_collection(valid_lines)
-		valid_corpus, _, _, _ = process_corpus(valid_sent_collection, mode = 'valid', vocab_dict = vocab_dict, label_dict = label_dict,
-			pos_dict = pos_dict, input_type = input_type)
+		valid_corpus, _, _= process_corpus(valid_sent_collection, mode = 'valid', vocab_dict = vocab_dict, label_dict = label_dict,
+			input_type = input_type)
 
 		if encoder == 'bert':
 			train_corpus = bert_tokenizer(train_corpus)
@@ -131,10 +128,10 @@ def dep_main(train_filename,
 		else:
 			input_type = 'lemma_ids'
 
-		arc_train(base_path = base_path, train_corpus = train_corpus, valid_corpus = valid_corpus, train_type = input_type, num_words = len(vocab_dict), 
-			num_pos = len(pos_dict), num_labels = len(label_dict), modelname = modelname, word_embed_size = word_embed_size, pos_embed_size = pos_embed_size, encoder = encoder,
-			lstm_hidden_size = lstm_hidden_size, lr = lr, dropout = dropout, num_epochs = num_epochs, lstm_layers = lstm_layers, 
-			batch_size = batch_size, bert = bert, bert_layer = bert_layer, scale = scale, typological = typological, typ_embed_size = typ_embed_size,
+		pos_train(base_path = base_path, train_corpus = train_corpus, valid_corpus = valid_corpus, train_type = input_type, num_words = len(vocab_dict), 
+			num_labels = len(label_dict), modelname = modelname, word_embed_size = word_embed_size, encoder = encoder,
+			lstm_hidden_size = lstm_hidden_size, mlp_hidden_size = mlp_hidden_size, lr = lr, dropout = dropout, num_epochs = num_epochs, lstm_layers = lstm_layers, 
+			batch_size = batch_size, bert = bert, bert_layer = bert_layer, typological = typological, typ_embed_size = typ_embed_size,
 			num_typ_features = num_typ_features, typ_feature = typ_feature, typ_encode = typ_encode, attention_hidden_size = attention_hidden_size, lang = lang, device = device)
 	else:
 		test_lines = preproc_conllu(file_path, filename = test_filename)
@@ -152,9 +149,9 @@ def dep_main(train_filename,
 		else:
 			input_type = 'lemma_ids'
 
-		print(arc_eval(base_path = base_path, test_corpus = test_corpus, eval_input = input_type, num_words = len(vocab_dict), num_pos = len(pos_dict), 
-			num_labels = len(label_dict), modelname = modelname, word_embed_size = word_embed_size, pos_embed_size = pos_embed_size, encoder = encoder,
-			lstm_hidden_size = lstm_hidden_size, dropout = dropout, lstm_layers = lstm_layers, bert = bert, bert_layer = bert_layer,
+		print(pos_eval(base_path = base_path, test_corpus = test_corpus, eval_input = input_type, num_words = len(vocab_dict),
+			num_labels = len(label_dict), modelname = modelname, word_embed_size = word_embed_size, encoder = encoder,
+			lstm_hidden_size = lstm_hidden_size, mlp_hidden_size = mlp_hidden_size, dropout = dropout, lstm_layers = lstm_layers, bert = bert, bert_layer = bert_layer,
 			scale = scale, typological = typological, typ_embed_size = typ_embed_size, typ_feature = typ_feature, num_typ_features = num_typ_features,
 			typ_encode = typ_encode, attention_hidden_size = attention_hidden_size, lang = lang, device = device))
 
@@ -189,10 +186,10 @@ if __name__ == '__main__':
 		train_model = args.train_model,
 		input_type = args.input_type,
 		word_embed_size = args.word_embed_size,
-		pos_embed_size = args.pos_embed_size,
 		modelname = args.modelname,
 		encoder = args.encoder,
 		lstm_hidden_size = args.lstm_hidden_size,
+		mlp_hidden_size = args.mlp_hidden_size,
 		lr = args.lr,
 		dropout = args.dropout,
 		num_epochs = args.num_epochs,
@@ -200,7 +197,6 @@ if __name__ == '__main__':
 		batch_size = 1,
 		bert = args.bert,
 		bert_layer = args.bert_layer,
-		scale = args.scale,
 		typological = args.typological,
 		typ_embed_size = args.typ_embed_size,
 		num_typ_features = args.num_typ_features,
